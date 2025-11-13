f431ad95fe04bfb7c044a0473eac772df3e1437e
Murillo Roberto Menegaldo de Carvalho <murillo.carvalho@mobiis.com.br>
2025-11-13 16:03:28 -0300

    Ajuste no main.py para criar banco automaticamente no Render

--- api/main.py
+++ api/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 from api import models
-from data.db import engine
+from data.db import engine, Base
 from api.routes import router as song_router
 from api.scraping_routes import router as scraping_router
 
-models.Base.metadata.create_all(bind=engine)
+# 🔥 Cria automaticamente o banco e as tabelas no Render
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Music Streaming API")
 
